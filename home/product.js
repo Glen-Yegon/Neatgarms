7fe864aa76addb46dfd5074b490afd8e40ab0a53
Glen_Yegon <yegonglen@gmail.com>
2025-04-03 05:09:28 +0300

    Removed js code in products page

--- home/product.js
+++ home/product.js
@@ -330,30 +330,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Get all images on the page
-  const images = document.querySelectorAll('img');
 
-  images.forEach(img => {
-      // Get the current image source
-      let src = img.getAttribute('src');
-      
-      // Check if the image is already a WebP, if not, convert it
-      if (!src.includes('.webp')) {
-          // Replace the file extension with .webp
-          let webpSrc = src.replace(/\.(jpg|jpeg|png)$/i, '.webp');
-          img.setAttribute('src', webpSrc);
-      }
-
-      // Set loading="lazy" to images for lazy loading
-      img.setAttribute('loading', 'lazy');
-
-      // Optionally, resize images if you want to enforce smaller sizes (optional)
-      // Example: If the image is wider than 800px, resize it
-      img.onload = function() {
-          if (img.width > 800) {
-              img.width = 800;
-          }
-      };
-  });
-});
