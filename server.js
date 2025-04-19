25a9379c5bcae6a00931577caaacda80896eb5b7
Glen_Yegon <yegonglen@gmail.com>
2025-04-19 04:09:21 +0300

    jej

--- server.js
+++ server.js
@@ -589,13 +589,13 @@ app.post("/pay3-now", async (req, res) => {
           <tr>
             <td align="center">
               <a href="https://neatgarms.com/product/urban-tee" target="_blank">
-                <img src="home/mains/playboy.jpg" alt="Urban Tee" width="140" style="border-radius: 8px;" />
+                <img src="https://neatgarms-six.vercel.app/mains/playboy.jpg" alt="Urban Tee" width="140" style="border-radius: 8px;" />
                 <p style="margin: 8px 0;">Urban Tee</p>
               </a>
             </td>
             <td align="center">
               <a href="https://neatgarms.com/product/classic-hoodie" target="_blank">
-                <img src="home/mains/travis.jpg" alt="Classic Hoodie" width="140" style="border-radius: 8px;" />
+                <img src="https://neatgarms-six.vercel.app/mains/travis.jpg" alt="Classic Hoodie" width="140" style="border-radius: 8px;" />
                 <p style="margin: 8px 0;">Classic Hoodie</p>
               </a>
             </td>
