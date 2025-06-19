038b49dc8f575b789ea2fcdc8f27aefd4a11346f
Glen_Yegon <yegonglen@gmail.com>
2025-06-19 04:07:57 +0300

    nkjdsa

--- home/scripts.js
+++ home/scripts.js
@@ -12,17 +12,3 @@ document.querySelectorAll('.product-card').forEach(card => {
   observer.observe(card);
 });
 
-// Testimonials
-// JavaScript is used to duplicate the cards for seamless looping
-document.addEventListener("DOMContentLoaded", () => {
-    const slider = document.querySelector(".testimonial-slider");
-    const cards = Array.from(slider.children);
-    
-    // Duplicate cards to ensure infinite scrolling effect
-    cards.forEach(card => {
-      const clone = card.cloneNode(true);
-      slider.appendChild(clone);
-    });
-  });
-
-
